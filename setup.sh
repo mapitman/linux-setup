82c29cb9cb32ed3cc2cb0255336da1a3f537da81
Mark Pitman <mapitman@users.noreply.github.com>
2015-07-05 15:13:56 -0700

    changed curl commands to not be silent

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ normal=$(tput sgr0)
 sudo apt-get install git mercurial curl vim-nox ctags
 
 # Setup zsh and oh-my-zsh
-curl -sSLo ~/ohmysh-install.sh https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+curl -Lo ~/ohmysh-install.sh https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 . ~/ohmyzsh-install.sh
 rm ~/ohmyzsh-install.sh
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"bira\"/g' ~/.zshrc
 
 # Setup Golang
-curl -sSO https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+curl -O https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
 mkdir ~/go
 mkdir ~/go/bin
@@ -37,12 +37,12 @@ if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
   echo "\033[0;33mFound ~/.vimrc.\033[0m \033[0;32mBacking up to ~/vimrc.old\033[0m";
   mv ~/.vimrc ~/vimrc.old;
 fi
-curl -sSLo ~/.vimrc https://raw.githubusercontent.com/mapitman/linux-setup/master/vimrc
+curl -Lo ~/.vimrc https://raw.githubusercontent.com/mapitman/linux-setup/master/vimrc
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -sSLo ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -Lo ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir -p ~/.vim/colors && \
-curl -sSLo ~/.vim/colors/atom-dark.vim https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark.vim
+curl -Lo ~/.vim/colors/atom-dark.vim https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/Shougo/neocomplete.vim.git ~/.vim/bundle/neocomplete
 git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
@@ -51,7 +51,7 @@ echo export EDITOR=\"vim\" >> ~/.zshrc
 echo export VISUAL=\"\$EDITOR\" >> ~/.zshrc
 
 # Setup Docker
-curl -sSLo ~/docker-setup.sh https://get.docker.com/
+curl -Lo ~/docker-setup.sh https://get.docker.com/
 . ~/docker-setup.sh
 rm ~/docker-setup.sh
 sudo usermod -aG docker $USER
