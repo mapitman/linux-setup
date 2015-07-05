a01a1336f04542bc10733297837990618e1ff070
Mark Pitman <mapitman@users.noreply.github.com>
2015-07-05 14:55:08 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ normal=$(tput sgr0)
 sudo apt-get install git mercurial curl vim-nox ctags
 
 # Setup zsh and oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+source < "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"bira\"/g' ~/.zshrc
 
 # Setup Golang
-curl -O https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+curl -sSO https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
 mkdir ~/go
 mkdir ~/go/bin
@@ -35,12 +35,12 @@ if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
   echo "\033[0;33mFound ~/.vimrc.\033[0m \033[0;32mBacking up to ~/vimrc.old\033[0m";
   mv ~/.vimrc ~/vimrc.old;
 fi
-curl -o ~/.vimrc https://gist.githubusercontent.com/mapitman/43c10e56bedb3b6e76ef/raw/0e0b2b3c8866c932df09e6e7b0704e6b03897f86/.vimrc
+curl -sSLo ~/.vimrc https://raw.githubusercontent.com/mapitman/linux-setup/master/vimrc
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -sSLo ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir -p ~/.vim/colors && \
-curl -Lo ~/.vim/colors/atom-dark.vim https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark.vim
+curl -sSLo ~/.vim/colors/atom-dark.vim https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/Shougo/neocomplete.vim.git ~/.vim/bundle/neocomplete
 git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
@@ -49,5 +49,5 @@ echo export EDITOR=\"vim\" >> ~/.zshrc
 echo export VISUAL=\"\$EDITOR\" >> ~/.zshrc
 
 # Setup Docker
-wget -qO- https://get.docker.com/ | sh
+source < "$(curl -sSL https://get.docker.com/)"
 sudo usermod -aG docker $USER
