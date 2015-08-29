149750365ffd4bde68986e5f0f76ea42b5fa7bb7
Mark Pitman <mapitman@users.noreply.github.com>
2015-08-29 10:46:13 -0700

    Update to go 1.5

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"bira\"/g' ~/.zshrc
 
 # Setup Golang
 curl -O https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.5.linux-amd64.tar.gz
 mkdir ~/go
 mkdir ~/go/bin
 mkdir ~/go/pkg
