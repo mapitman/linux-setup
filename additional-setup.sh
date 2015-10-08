1a3d6dffed801cc9f16d0d8b4335cbc600bac03e
Mark Pitman <mapitman@users.noreply.github.com>
2015-10-07 23:19:01 -0700

    added vala

--- additional-setup.sh
+++ additional-setup.sh
@@ -6,10 +6,13 @@ sudo su
 # HipChat - http://www.hipchat.com
 echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
   /etc/apt/sources.list.d/atlassian-hipchat.list
-wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+wget -O - https://www.hpchat.com/keys/hipchat-linux.key | apt-key add -
 
 # FeedReader - https://jangernert.github.io/feedreader/
 add-apt-repository ppa:eviltwin1/feedreader-stable
+
+# Vala - https://wiki.gnome.org/Projects/Vala/
+add-apt-repository ppa:vala-team/ppa
 
 apt-get update
 apt-get install feedreader hipchat
