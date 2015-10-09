4ee1476c445a716dd0c3b9adf2450bcd27a51bb7
Mark Pitman <mapitman@users.noreply.github.com>
2015-10-09 00:34:38 -0700

    Update additional-setup.sh

--- additional-setup.sh
+++ additional-setup.sh
@@ -24,3 +24,5 @@ apt-get install feedreader hipchat build-essential valac autoconf autogen libtoo
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i --force-depends ./google-chrome-stable_current_amd64.deb
 apt-get install -f
+
+exit
