3f91dca7db1b10a0af6e46d5ac28f1a2fc630805
Mark Pitman <mapitman@users.noreply.github.com>
2015-10-09 00:34:21 -0700

    add x2go

--- additional-setup.sh
+++ additional-setup.sh
@@ -14,8 +14,11 @@ add-apt-repository ppa:eviltwin1/feedreader-stable
 # Vala - https://wiki.gnome.org/Projects/Vala/
 add-apt-repository ppa:vala-team/ppa
 
+# x2go - http://wiki.x2go.org/doku.php
+add-apt-repository ppa:x2go/stable
+
 apt-get update
-apt-get install feedreader hipchat build-essential valac autoconf autogen libtool flex bison
+apt-get install feedreader hipchat build-essential valac autoconf autogen libtool flex bison x2goserver x2goserver-xsession
 
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
