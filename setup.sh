f7ce3046bbd99b0cc1227aab82c931040e86a696
Mark Pitman <mapitman@users.noreply.github.com>
2015-11-24 23:18:46 -0800

    Fix typo for hipchat.com

--- setup.sh
+++ setup.sh
@@ -62,12 +62,12 @@ source ~/.zshrc
 nvm install stable
 npm install -g gulp
 
-# rest of script will run as root
-sudo su
+
 # HipChat - http://www.hipchat.com
+sudo su
 echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
   /etc/apt/sources.list.d/atlassian-hipchat.list
-wget -O - https://www.hpchat.com/keys/hipchat-linux.key | apt-key add -
+wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
 apt-get update
 apt-get install hipchat
 
