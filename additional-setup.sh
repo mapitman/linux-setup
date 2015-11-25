6d06d1626d7af13e381b7afa0ab8f1d476de9a06
Mark Pitman <mapitman@users.noreply.github.com>
2015-11-24 22:41:40 -0800

    Update additional setup
    Add Broadcom WiFi drivers
    Remove X2GO, Vala, Feedreader since I don't use them anymore

--- additional-setup.sh
+++ additional-setup.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 sudo su
 
-# install new PPAs
-
 # HipChat - http://www.hipchat.com
 echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
   /etc/apt/sources.list.d/atlassian-hipchat.list
 wget -O - https://www.hpchat.com/keys/hipchat-linux.key | apt-key add -
-
-# FeedReader - https://jangernert.github.io/feedreader/
-add-apt-repository ppa:eviltwin1/feedreader-stable
-
-# Vala - https://wiki.gnome.org/Projects/Vala/
-add-apt-repository ppa:vala-team/ppa
-
-# x2go - http://wiki.x2go.org/doku.php
-add-apt-repository ppa:x2go/stable
-
 apt-get update
-apt-get install feedreader hipchat build-essential valac autoconf autogen libtool flex bison x2goserver x2goserver-xsession
+apt-get install hipchat
+
+# install some build tools and the Broadcom WiFi driver for MacBooks
+apt-get install build-essential autoconf autogen libtool flex bison bcmwl-kernel-source
 
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
