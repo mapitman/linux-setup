7739cc6f4d3d707e316413f8f994b4293d8176f0
Mark Pitman <mapitman@users.noreply.github.com>
2015-11-24 22:47:48 -0800

    Move stuff from additional setup file into setup.sh

--- setup.sh
+++ setup.sh
@@ -61,3 +61,22 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | b
 source ~/.zshrc
 nvm install stable
 npm install -g gulp
+
+# rest of script will run as root
+sudo su
+# HipChat - http://www.hipchat.com
+echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
+  /etc/apt/sources.list.d/atlassian-hipchat.list
+wget -O - https://www.hpchat.com/keys/hipchat-linux.key | apt-key add -
+apt-get update
+apt-get install hipchat
+
+# install some build tools and the Broadcom WiFi driver for MacBooks
+apt-get install build-essential autoconf autogen libtool flex bison bcmwl-kernel-source
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i --force-depends ./google-chrome-stable_current_amd64.deb
+apt-get install -f
+
+exit
