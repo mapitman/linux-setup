793f9a05ddeaf4b44c35590305484a2383251978
Mark Pitman <mapitman@users.noreply.github.com>
2016-05-18 20:25:28 -0700

    Remove HipChat

--- additional-setup.sh
+++ additional-setup.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 sudo su
 
-# HipChat - http://www.hipchat.com
-echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
-  /etc/apt/sources.list.d/atlassian-hipchat.list
-wget -O - https://www.hpchat.com/keys/hipchat-linux.key | apt-key add -
-apt-get update
-apt-get install hipchat
-
 # install some build tools and the Broadcom WiFi driver for MacBooks
 apt-get install build-essential autoconf autogen libtool flex bison bcmwl-kernel-source
 
