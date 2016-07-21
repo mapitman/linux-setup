ed0f33f38439bbe14b9394b12744258287004bca
Mark Pitman <mapitman@users.noreply.github.com>
2016-07-20 23:16:24 -0700

    Add unity tweak tool

--- additional-setup.sh
+++ additional-setup.sh
@@ -4,6 +4,9 @@ sudo su
 # install some build tools and the Broadcom WiFi driver for MacBooks
 apt-get install build-essential autoconf autogen libtool flex bison bcmwl-kernel-source
 
+# install unity tweak tool
+apt-get install unity-tweak-tool
+
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i --force-depends ./google-chrome-stable_current_amd64.deb
