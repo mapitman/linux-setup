7abfa3886169d70ec258713ab70f343b23c4511c
Mark Pitman <mapitman@users.noreply.github.com>
2016-07-20 23:17:20 -0700

    add tlp for power management

--- additional-setup.sh
+++ additional-setup.sh
@@ -7,6 +7,10 @@ apt-get install build-essential autoconf autogen libtool flex bison bcmwl-kernel
 # install unity tweak tool
 apt-get install unity-tweak-tool
 
+# install tlp for power management
+apt-get install tlp
+tlp start
+
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i --force-depends ./google-chrome-stable_current_amd64.deb
