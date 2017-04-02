3719ea7689ca77eed0d7b784e2d5b780b4f9e2fd
Mark Pitman <mapitman@users.noreply.github.com>
2017-04-02 15:01:46 -0700

    use apt instead of apt-get

--- additional-setup.sh
+++ additional-setup.sh
@@ -2,18 +2,18 @@
 sudo su
 
 # install some build tools
-apt-get install build-essential autoconf autogen libtool flex bison
+apt -y install build-essential autoconf autogen libtool flex bison
 
 # install unity tweak tool
-apt-get install unity-tweak-tool
+apt -y install unity-tweak-tool
 
 # install tlp for power management
-apt-get install tlp
+apt -y install tlp
 tlp start
 
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i --force-depends ./google-chrome-stable_current_amd64.deb
-apt-get install -f
+apt install -f
 
 exit
