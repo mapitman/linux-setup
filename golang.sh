08d3adad4bb0005b77289102fa93f6d5a5b744e5
Mark Pitman <mark.pitman@gmail.com>
2018-06-24 14:33:35 -0700

    Add more golang tools

--- golang.sh
+++ golang.sh
@@ -14,14 +14,25 @@ then
     echo PATH=\"\$PATH:\$HOME/bin:/usr/local/go/bin:\$GOPATH/bin\" >> ~/.bashrc
 fi
 
-echo "Installing extra golang tools..."
+echo "go get -u extra golang tools..."
 GOPATH="$HOME/go"
 PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
 go get -u github.com/jstemmer/gotags
-go get -u github.com/nsf/gocode
+go get -u github.com/mdempsky/gocode
 go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/rogpeppe/godef
 go get -u golang.org/x/tools/cmd/guru
 go get -u golang.org/x/tools/cmd/gorename
 go get -u github.com/golang/lint/golint
 go get -u github.com/kisielk/errcheck
+go get -u github.com/uudashr/gopkgs/cmd/gopkgs
+go get -u github.com/ramya-rao-a/go-outline
+go get -u github.com/acroca/go-symbols
+go get -u golang.org/x/tools/cmd/godoc
+go get -u github.com/sqs/goreturns
+go get -u github.com/derekparker/delve/cmd/dlv
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/haya14busa/goplay/cmd/goplay
+go get -u github.com/josharian/impl
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/cweill/gotests/...
