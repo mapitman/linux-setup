a270346afaa08144612872b4665b9af4fc99975d
Mark Pitman <mark.pitman@gmail.com>
2018-09-03 11:49:12 -0700

    Add kubectl setup

---
+++ kubectl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Install kubectl to control and deploy to a Keubernetes cluster
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
