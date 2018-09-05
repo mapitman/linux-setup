c2fae4fb39b7e180bfa140d1193fb260c3def0f8
Mark Pitman <mark.pitman@gmail.com>
2018-09-04 23:51:19 -0700

    Update libvirt setup

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -67,7 +67,10 @@ sudo snap install --classic kubectl
 
 # minikube
 sudo apt-get install -y qemu-kvm libvirt-bin virt-top libguestfs-tools virtinst bridge-utils
+sudo systemctl enable virtlockd.service
+sudo systemctl start virtlockd.service
 sudo modprobe vhost_net
-lsmod | grep vhost
-sudo echo vhost_net >> /etc/modules
-
+sudo lsmod | grep vhost
+sudo -i
+echo vhost_net >> /etc/modules
+exit
