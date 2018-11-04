250fb4260fd93d499bf4bc7896cce193c66f50a3
Mark Pitman <mark.pitman@gmail.com>
2018-11-03 19:53:45 -0700

    Add htop

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,7 @@
 # install necessary packages
 sudo apt-get -y install mercurial curl vim-nox ctags git-extras docker.io autojump \
 build-essential autoconf autogen libtool flex bison openconnect xclip pwgen ranger \
-dialog yadm fonts-firacode python-pip pylint pandoc tmux
+dialog yadm fonts-firacode python-pip pylint pandoc tmux htop
 
 sudo snap install vscode --classic
 sudo snap install slack --classic
