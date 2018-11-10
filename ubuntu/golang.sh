6c31b77145421c2cf2d0d8fcac968c496778856e
Mark Pitman <mark.pitman@gmail.com>
2018-11-09 17:35:51 -0800

    Update golang version

--- ubuntu/golang.sh
+++ ubuntu/golang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gopkg=go1.11.linux-amd64.tar.gz
+gopkg=go1.11.2.linux-amd64.tar.gz
 curl -O https://storage.googleapis.com/golang/$gopkg
 sudo tar -C /usr/local -xzf $gopkg
 rm $gopkg
