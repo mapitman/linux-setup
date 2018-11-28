1098e0edf5efcff9ccff8aabe0e6faa6b37ef37c
Mark Pitman <mark.pitman@gmail.com>
2018-11-28 13:04:11 -0800

    Updates to auto-install packages after keybase install and new package names for libvirt

--- ubuntu/desktop/keybase.sh
+++ ubuntu/desktop/keybase.sh
@@ -3,6 +3,6 @@
 keybasepkg=keybase_amd64.deb
 curl -O https://prerelease.keybase.io/$keybasepkg
 sudo dpkg -i $keybasepkg
-sudo apt-get install -f
+sudo apt-get install -f -y
 run_keybase
 rm $keybasepkg
--- ubuntu/kubernetes.sh
+++ ubuntu/kubernetes.sh
@@ -4,11 +4,10 @@
 sudo snap install --classic kubectl
 
 # minikube
-sudo apt-get install -y qemu-kvm libvirt-bin virt-top libguestfs-tools virtinst bridge-utils
+sudo apt-get install -y qemu qemu-kvm virt-manager virt-top libguestfs-tools virtinst bridge-utils
 sudo systemctl enable virtlockd.service
 sudo systemctl start virtlockd.service
 sudo modprobe vhost_net
 lsmod | grep vhost
-sudo -i
-echo vhost_net >> /etc/modules
-exit
+echo 'vhost_net' | sudo tee --append /etc/modules > /dev/null
+
