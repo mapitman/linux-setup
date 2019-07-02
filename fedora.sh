58741a18eee7a6954e85c69dab9e2412e8c59fc8
Mark Pitman <mark.pitman@gmail.com>
2019-07-02 13:02:00 -0700

    Add more packages to install on Fedora

--- fedora.sh
+++ fedora.sh
@@ -3,7 +3,7 @@
 sudo dnf update
 sudo dnf upgrade
 sudo dnf -y copr enable thelocehiliosan/yadm
-sudo dnf install -y snapd mercurial vim ctags git-extras docker autojump make autoconf autogen libtool flex bison openconnect pwgen ranger dialog yadm python3-pip pylint pandoc tmux htop jq golang bat python3-pypandoc twine python3-autopep8 kubernetes-client
+sudo dnf install -y snapd mercurial vim ctags git-extras docker autojump make autoconf autogen libtool flex bison openconnect pwgen ranger dialog yadm python3-pip pylint pandoc tmux htop jq golang bat python3-pypandoc twine python3-autopep8 kubernetes-client NetworkManager-tui light fontawesome-fonts python2-psutil python2-netifaces python2-requests
 
 for file in ./fedora/*.sh
 do
