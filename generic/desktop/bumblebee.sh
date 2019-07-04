724b33dd06ddcc7b1a5a327b50146a7774af8df6
Mark Pitman <mark@phoenix.pitman.local>
2019-07-03 22:18:55 -0700

    Fix Fedora setup

--- generic/desktop/bumblebee.sh
+++ generic/desktop/bumblebee.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p $HOME/src/github/tobi-wan-kenobi
-cd $HOME/src/github/tobi-wan-kenobi
+pushd $HOME/src/github/tobi-wan-kenobi
 git clone git://github.com/tobi-wan-kenobi/bumblebee-status
+popd
 
