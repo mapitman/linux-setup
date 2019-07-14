bac125ba9e1ec7db38b03ad4cd4b8f23d2a943b6
Mark Pitman <mark.pitman@gmail.com>
2019-07-14 16:34:05 -0700

    Add ffmpeg install for fedora

---
+++ fedora/desktop/ffmpeg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf -y install ffmpeg ffmpeg-devel
