5e21dacae25521a0769451fe2bfee557f9a090f1
Mark Pitman <mapitman@users.noreply.github.com>
2019-12-11 11:30:52 -0800

    Update git.sh
    Add comment with command to install packages

--- generic/git.sh
+++ generic/git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # On Ubuntu install these packages first libssl-dev zlib1g-dev libcurl4-openssl-dev
+# e.g. sudo apt install libssl-dev zlib1g-dev libcurl4-openssl-dev
 
 # This will end up installing git in your home directory which will not
 # interfere with the files installed by your OS package manager
