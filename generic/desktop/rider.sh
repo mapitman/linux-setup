7b95f31f8b0f9c0fcabc8d0dbd413a7edb3221cd
Mark Pitman <mark@pitman.io>
2020-01-13 11:05:21 -0800

    Fix a couple of files
    Make rider install not use sudo
    add +x to yadm.sh

--- generic/desktop/rider.sh
+++ generic/desktop/rider.sh
@@ -2,5 +2,5 @@
 
 curl -L -o /tmp/rider.tar.gz https://download.jetbrains.com/rider/JetBrains.Rider-2019.2.3.tar.gz
 mkdir $HOME/rider
-sudo tar xfz /tmp/rider.tar.gz -C $HOME/rider --strip-components 1
+tar xfz /tmp/rider.tar.gz -C $HOME/rider --strip-components 1
 
