bfd4d3405ef86684b5d202cb2aab225a534330a1
Mark Pitman <mark@pitman.io>
2020-02-28 10:54:54 -0800

    Update to latest version of Rider

--- generic/desktop/rider.sh
+++ generic/desktop/rider.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o /tmp/rider.tar.gz https://download.jetbrains.com/rider/JetBrains.Rider-2019.3.1.tar.gz
-mkdir $HOME/rider
+curl -L -o /tmp/rider.tar.gz https://download.jetbrains.com/rider/JetBrains.Rider-2019.3.4.tar.gz
+mkdir -p $HOME/rider
 tar xfz /tmp/rider.tar.gz -C $HOME/rider --strip-components 1
 
