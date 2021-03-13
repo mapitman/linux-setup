9f26fdf65cdbda63344ac21eaab28ba6df095256
Mark Pitman <mark@pitman.io>
2021-03-13 13:57:49 -0800

    Add dotnet 5.0

--- generic/dotnet.sh
+++ generic/dotnet.sh
@@ -5,5 +5,6 @@ chmod +x /tmp/dotnet-install.sh
 
 /tmp/dotnet-install.sh -c 2.1
 /tmp/dotnet-install.sh -c 3.1
+/tmp/dotnet-install.sh -c 5.0
 
 $HOME/.dotnet/dotnet tool install --global PowerShell
