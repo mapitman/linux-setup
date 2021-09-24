382d6f1d31f09ce782c2ede10dc60f25f22080f4
Mark Pitman <mark@pitman.io>
2021-09-24 16:30:57 -0700

    Add zsh stuff

--- generic/ohmyzsh.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
---
+++ generic/zsh-customizations.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# plugins
+# Autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -rf  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/.git
+rm -rf  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/.github
+rm -rf  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/.circleci
+
+# Syntax Highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/.git
+
+
+# Pure prompt
+mkdir -p "$HOME/.zsh"
+git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+rm -rf $HOME/.zsh/pure/.git
