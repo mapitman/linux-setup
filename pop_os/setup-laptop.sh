dbef6a983d674e3001e797901a26582aa62089f8
Mark Pitman <mark@pitman.io>
2022-05-01 14:53:54 -0700

    Update pop_os setup

--- pop_os/setup-laptop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# tlp for power management
-sudo apt-get -y install tlp
-sudo tlp start
--- pop_os/setup.sh
+++ pop_os/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository universe
+
 # Add .NET PPA
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -12,31 +14,29 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y apt-transport-https && sudo apt-get update
 
 # install necessary packages
-sudo apt-get -y install make mercurial curl vim-nox universal-ctags \
+sudo apt-get -y install make mercurial curl neovim universal-ctags \
 build-essential autoconf autogen libtool flex bison pwgen ranger \
 dialog python3-pip pylint pandoc htop jq renameutils checkinstall \
 libssl-dev zlib1g-dev libcurl4-openssl-dev nmap pandoc \
 twine python3-autopep8 pv zfsutils-linux zsh zsh-doc neofetch \
 imagemagick openjdk-11-jdk dotnet-sdk-6.0 \
-bat hugo yadm git-extras
-
-if uname -a | grep -i -v -q Microsoft
-then
-    sudo apt-get -y install docker.io tmux openconnect
-fi
+bat hugo yadm git-extras python-is-python3 docker.io tmux openconnect \
+audacity darktable evolution evolution-ews ffmpeg libavcodec-dev \
+fonts-firacode gimp gnome-tweaks kdiff3 obs-studio xclip \
+libavcodec-extra ubuntu-restricted-extras code fonts-cascadia-code \
+caffeine
 
 source ./generic/zsh-customizations.sh
 
-read -p "Install desktop apps? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    source ./pop_os/setup-desktop.sh
-fi
-
-read -p "Install laptop apps? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    source ./pop_os/setup-laptop.sh
-fi
+dconf load /org/gnome/shell/keybindings/ <<EOF
+[/]
+switch-to-application-1=['<Super>1']
+switch-to-application-2=['<Super>2']
+switch-to-application-3=['<Super>3']
+switch-to-application-4=['<Super>4']
+switch-to-application-5=['<Super>5']
+switch-to-application-6=['<Super>6']
+switch-to-application-7=['<Super>7']
+switch-to-application-8=['<Super>8']
+switch-to-application-9=['<Super>9']
+EOF
