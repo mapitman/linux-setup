86226952afb1469565f69728a58d920de8f7044b
Mark Pitman <mark@pitman.io>
2023-06-06 00:02:47 -0700

    Use nala in more places

--- ubuntu/setup-desktop.sh
+++ ubuntu/setup-desktop.sh
@@ -18,18 +18,6 @@ then
 curl -k -L -o ~/.config/aacs/KEYDB.cfg http://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg
 fi
 
-# Install Pop!_OS packages
-# if grep -Fq "Pop" /etc/os-release
-# then
-#     sudo apt install -y gnome-shell-extension-dash-to-panel slack-desktop
-# else
-#     # slack
-#     curl -o /tmp/slack-desktop-4.4.2-amd64.deb  https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.2-amd64.deb
-#     sudo dpkg -i /tmp/slack-desktop-4.4.2-amd64.deb
-#     sudo apt install -fy
-# fi
-
-
 # Install cascadia code font
 
 # curl -L -o /tmp/cascadia-code.zip https://github.com/microsoft/cascadia-code/releases/download/v2007.01/CascadiaCode-2007.01.zip
--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt install -y curl nala
+sudo apt-get update && sudo apt-get install -y curl nala
 
 # Add .NET PPA
 curl -O https://packages.microsoft.com/config/ubuntu/23.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-sudo apt-get install wget gpg
+sudo nala -y install wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
