ca2f7f682ddec8c2b32890cf62af42ae0ef40050
Mark Pitman <mark@pitman.io>
2023-06-10 17:22:32 -0700

    Remove outdated gnome extensions

--- ubuntu/desktop/gnome-extensions.sh
+++ ubuntu/desktop/gnome-extensions.sh
@@ -5,13 +5,9 @@ pipx install gnome-extensions-cli --system-site-packages
 gext install caffeine@patapon.info
 gext install blur-my-shell@aunetx
 gext install clipboard-history@alexsaveau.dev
-gext install nvidiautil@ethanwharris
-gext install sensory-perception@HarlemSquirrel.github.io
 gext install tiling-assistant@leleat-on-github
 
 gext enable caffeine@patapon.info
 gext enable blur-my-shell@aunetx
 gext enable clipboard-history@alexsaveau.dev
-gext enable nvidiautil@ethanwharris
-gext enable sensory-perception@HarlemSquirrel.github.io
-gext enable tiling-assistant@leleat-on-github
+gext enable tiling-assistant@leleat-on-githu
