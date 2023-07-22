57da2fab3e14db84804a75a9000ccb19b98be8cf
Mark Pitman <mark@pitman.io>
2023-07-21 17:40:16 -0700

    Switch to WinTile tiling extension

--- ubuntu/desktop/gnome-extensions.sh
+++ ubuntu/desktop/gnome-extensions.sh
@@ -5,11 +5,11 @@ pipx install gnome-extensions-cli --system-site-packages
 gext install caffeine@patapon.info
 gext install blur-my-shell@aunetx
 gext install clipboard-history@alexsaveau.dev
-gext install tiling-assistant@leleat-on-github
 gext install freon@UshakovVasilii_Github.yahoo.com
+gext install wintile@nowsci.com
 
 gext enable caffeine@patapon.info
 gext enable blur-my-shell@aunetx
 gext enable clipboard-history@alexsaveau.dev
-gext enable tiling-assistant@leleat-on-github
 gext enable freon@UshakovVasilii_Github.yahoo.com
+gext enable wintile@nowsci.com
