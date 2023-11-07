ad7fa62f8c5589a665f8be41a79e0703bf04c9ac
Mark Pitman <mark@pitman.io>
2023-11-06 19:30:17 -0800

    Remove wintile extension since Gnome 45 has some tiling features

--- ubuntu/desktop/gnome-extensions.sh
+++ ubuntu/desktop/gnome-extensions.sh
@@ -7,10 +7,8 @@ pipx install gnome-extensions-cli --system-site-packages
 "$HOME"/.local/bin/gext install blur-my-shell@aunetx
 "$HOME"/.local/bin/gext install clipboard-history@alexsaveau.dev
 "$HOME"/.local/bin/gext install freon@UshakovVasilii_Github.yahoo.com
-"$HOME"/.local/bin/gext install wintile@nowsci.com
 
 "$HOME"/.local/bin/gext enable caffeine@patapon.info
 "$HOME"/.local/bin/gext enable blur-my-shell@aunetx
 "$HOME"/.local/bin/gext enable clipboard-history@alexsaveau.dev
 "$HOME"/.local/bin/gext enable freon@UshakovVasilii_Github.yahoo.com
-"$HOME"/.local/bin/gext enable wintile@nowsci.com
